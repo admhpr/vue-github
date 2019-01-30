e91c6187c4a880b94e724bda07ddf39229dce9be
Adam Harpur <adam@harpur.io>
2019-01-29 20:53:59 -0600

    feat: columns for Issues Event

--- src/components/feed/events/Issue.vue
+++ src/components/feed/events/Issue.vue
@@ -1,15 +1,44 @@
 <template>
-    <div>
-         Issue
+  <div class="row" v-if="event">
+    <div style="display: inline-block;" class="column">
+      <span
+        class="event-octicon octicon dashboard-event-icon"
+        :class="event.payload.action === 'opened'?'octicon-issue-opened':'octicon-issue-closed'"
+      ></span>
+      {{event.payload.action}} issue
+      <a
+        class="event-link"
+        :href="event.payload.issue.html_url"
+        target="_blank"
+      >#{{event.payload.issue.number}}</a>
+      at
+      <a
+        class="event-link"
+        :href="'https://github.com/' + event.repo.name"
+        target="_blank"
+      >{{event.repo.name}}</a>
     </div>
+    <div class="column">{{daysAgo(event.created_at)}}</div>
+    <div class="column">
+      <ul>
+        <li>
+          <span class="event-octicon octicon octicon-chevron-right dashboard-event-icon"></span>
+          {{event.payload.issue.title}}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import { daysAgo } from "../../utils/format";
+export default {
+  name: "watch-event",
+  props: {
+    event: { required: true }
+  },
+  methods: {
+    daysAgo
+  }
+};
 </script>
-
-<style scoped>
-
-</style>
