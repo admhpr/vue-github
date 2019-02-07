b40904c1d170fdbbf1b444e0c0f918f6ee4a3cec
Adam Harpur <adam@harpur.io>
2019-02-06 20:24:19 -0600

    feat: added public event component

--- src/components/feed/events/Public.vue
+++ src/components/feed/events/Public.vue
@@ -1,5 +1,13 @@
 <template>
-  <event-container :event="event"></event-container>
+  <event-container :event="event">
+      <div slot="activity">
+        <span class="event-octicon octicon octicon-radio-tower dashboard-event-icon"></span>
+        <a>{{event.actor.login}}</a>
+        open sourced 
+        <a class="event-link" :href="'https://github.com/' + event.repo.name" target="_blank">{{event.repo.name}}</a>
+      </div>
+      <li slot="list-info"></li>
+  </event-container>
 </template>
 
 <script>
