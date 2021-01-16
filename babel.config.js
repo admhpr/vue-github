eae2f3060fae084dbe2a7a183d91dc3893a2acfc
adam <adam@harpur.io>
2021-01-16 16:23:41 +0000

    chore: update packages

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
-    '@vue/app'
-  ]
+    "@vue/app",
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: 3,
+      },
+    ],
+  ],
 }
