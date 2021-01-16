0d0ac9c96c0bf5b95075b1e05781b99b287a1a73
adam <adam@harpur.io>
2021-01-16 16:30:16 +0000

    style: add vars

--- src/components/Github.vue
+++ src/components/Github.vue
@@ -4,16 +4,17 @@
       <img
         src="https://github.githubassets.com/images/spinners/octocat-spinner-128.gif"
         class="spinner"
-      >
+      />
       <p class="spinner-text monospace">
         Crunching
-        <a :href="'https://github.com/' + username">@{{username}}</a>'s contributions just for you.
+        <a :href="'https://github.com/' + username">@{{ username }}</a
+        >'s contributions just for you.
       </p>
     </div>
     <div v-if="!loading">
       <div class="activity-calendar-wrapper">
         <div v-if="showCalendar" v-html="this.rawCalendar.innerHTML"></div>
-        <br>
+        <br />
         <github-feed v-if="showFeed" username="harps116"></github-feed>
       </div>
     </div>
@@ -21,107 +22,106 @@
 </template>
 
 <script>
-import parse from "github-calendar-parser";
-import GithubFeed from "./feed/GithubFeed.vue";
-import { debounce } from "./utils/utils";
+import parse from "github-calendar-parser"
+import GithubFeed from "./feed/GithubFeed.vue"
+import { debounce } from "./utils/utils"
 export default {
   props: {
     username: { String, required: true },
     text: String,
     proxy: Function,
     showFeed: { Boolean, default: true },
-    showCalendar: { Boolean, default: true }
+    showCalendar: { Boolean, default: true },
   },
   components: {
-    GithubFeed
+    GithubFeed,
   },
   data: function() {
     return {
       loading: true,
       rawCalendar: "",
       rawSvg: "",
-      builtCalendar: ""
-    };
+      builtCalendar: "",
+    }
   },
   mounted() {
-    this.createCalendar();
+    this.createCalendar()
   },
   watch: {
     text: {
       handler(val, oldVal) {
         if (val !== oldVal) {
-          debounce(this.createCalendar(), 100);
+          debounce(this.createCalendar(), 100)
         }
-      }
-    }
+      },
+    },
   },
   methods: {
     createCalendar: function() {
-      this.fetchCalendar();
+      this.fetchCalendar()
     },
     fetchCalendar: function() {
       const proxy =
         this.proxy ||
         function(url) {
-          return "https://urlreq.appspot.com/req?method=GET&url=" + url;
-        };
+          return "https://urlreq.appspot.com/req?method=GET&url=" + url
+        }
 
       // We need a proxy for CORS
       // Thanks, @izuzak (https://github.com/izuzak/urlreq)
       fetch(proxy(`https://github.com/${this.username}`))
-        .then(res => res.text())
-        .then(body => {
-          this.setupCalendar(body);
+        .then((res) => res.text())
+        .then((body) => {
+          this.setupCalendar(body)
           if (this.rawCalendar.querySelector("includes-fragment")) {
-            setTimeout(this.fetchCalendar, 500);
+            setTimeout(this.fetchCalendar, 500)
           } else {
-            this.renderCalendar();
+            this.renderCalendar()
           }
-        });
+        })
     },
     renderCalendar: async function() {
-      await this.setupSvg();
-      await this.parseSvg();
+      await this.setupSvg()
+      await this.parseSvg()
     },
     setupCalendar: function(body) {
-      let div = document.createElement("div");
+      let div = document.createElement("div")
       let summary =
         this.text ||
-        `Summary of pull requests, issues opened, and commits made by <a href="https://github.com/${
-          this.username
-        }" target="blank">@${this.username}</a>`;
-      div.innerHTML = body;
-      let cal = div.querySelector(".js-yearly-contributions");
-      let summaryText = cal.querySelector(".float-left.text-gray");
-      summaryText.innerHTML = summary;
-      summaryText.className = "";
-      summaryText.className = "summary-text";
-      cal.querySelector(".contrib-legend").innerHTML = "";
-      this.rawCalendar = cal;
+        `Summary of pull requests, issues opened, and commits made by <a href="https://github.com/${this.username}" target="blank">@${this.username}</a>`
+      div.innerHTML = body
+      let cal = div.querySelector(".js-yearly-contributions")
+      let summaryText = cal.querySelector(".float-left.text-gray")
+      summaryText.innerHTML = summary
+      summaryText.className = ""
+      summaryText.className = "summary-text"
+      cal.querySelector(".contrib-legend").innerHTML = ""
+      this.rawCalendar = cal
     },
     setupSvg: function() {
-      let svg = this.rawCalendar.querySelector("svg.js-calendar-graph-svg");
-      let width = svg.getAttribute("width");
-      let height = svg.getAttribute("height");
+      let svg = this.rawCalendar.querySelector("svg.js-calendar-graph-svg")
+      let width = svg.getAttribute("width")
+      let height = svg.getAttribute("height")
       // Dimension adjustment
-      svg.removeAttribute("height");
-      svg.setAttribute("width", "100%");
-      svg.setAttribute("viewBox", "0 0 " + width + " " + height);
-      this.rawSvg = svg;
+      svg.removeAttribute("height")
+      svg.setAttribute("width", "100%")
+      svg.setAttribute("viewBox", "0 0 " + width + " " + height)
+      this.rawSvg = svg
     },
     parseSvg: function() {
-      let parsed = parse(String(this.rawSvg.outerHTML));
-      this.buildCalendar(parsed);
+      let parsed = parse(String(this.rawSvg.outerHTML))
+      this.buildCalendar(parsed)
     },
     buildCalendar: function(data) {
-      this.contribData = data;
-      this.loading = false;
-    }
-  }
-};
+      this.contribData = data
+      this.loading = false
+    },
+  },
+}
 </script>
 
 <style lang="scss">
+@import "../assets/vars.scss"; 
 $green: #42b983;
 $grey: #dddddd;
 
