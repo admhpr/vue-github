8f18bee3e89fb8094f10f1f2367ae2b8412c0d28
adam <adam@harpur.io>
2021-01-19 17:43:40 +0000

    refactor: update utils

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,46 +1,46 @@
-const fromNow = isoDate => {
-  const createdAt = new Date(isoDate);
-  const now = new Date();
-  const timeDiff = Math.abs(createdAt.getTime() - now.getTime());
-  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); // milliseconds in day
-  return diffDays - 1;
-};
+function fromNow(isoDate) {
+  const createdAt = new Date(isoDate)
+  const now = new Date()
+  const timeDiff = Math.abs(createdAt.getTime() - now.getTime())
+  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)) // milliseconds in day
+  return diffDays - 1
+}
 
-const hash = sha => {
-  return sha.substr(sha.length - 6);
-};
+function hash(sha) {
+  return sha.substr(sha.length - 6)
+}
 
-const branch = ref => {
-  return ref.replace("refs/heads/", "");
-};
+function branch(ref) {
+  return ref.replace("refs/heads/", "")
+}
 
-const daysAgo = isoDate => {
-  const diff = fromNow(isoDate);
-  const isPlural = diff > 1 ? "s" : "";
-  return diff === 0 ? `today` : `${diff} day${isPlural} ago`;
-};
+function daysAgo(isoDate) {
+  const diff = fromNow(isoDate)
+  const isPlural = diff > 1 ? "s" : ""
+  return diff === 0 ? `today` : `${diff} day${isPlural} ago`
+}
 
 // @see https://davidwalsh.name/javascript-debounce-function
 
-const debounce = (func, wait, immediate) => {
-  let timeout;
+function debounce(func, wait, immediate) {
+  let timeout
   return () => {
     let delay = () => {
-      timeout = null;
+      timeout = null
       if (!immediate) {
-        func();
+        func()
       }
-    };
-    let callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(delay, wait);
-    if (callNow) {
-      func();
     }
-  };
-};
+    let callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(delay, wait)
+    if (callNow) {
+      func()
+    }
+  }
+}
 
 function getLink({ name }) {
-  return `https://github.com/${name}`;
+  return `https://github.com/${name}`
 }
-export { daysAgo, debounce, getLink, hash, branch };
+export { daysAgo, debounce, getLink, hash, branch }
